06f976db8be2c7b5c2717d9abca8582b888b2653
Anish Gopal <anishgopal.19@gmail.com>
2022-04-17 13:26:59 +0530

    Added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+class Multiply{
+    public static void main(String[] args) {
+        
+        int a = 5;
+        int b = 6;
+        System.out.println("The product of "+a+" & "+b+" is "+a*b);
+    }
+}
